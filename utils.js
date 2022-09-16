193db2aae83ece7ee970068ec322c2c0fe50149e
Kizmelvin <chidiebere.johnbosco@gmail.com>
2022-09-16 16:26:43 +0100

    initial commit

---
+++ utils.js
@@ -0,0 +1,6 @@
+import Client from "shopify-buy";
+
+export const client = Client.buildClient({
+  storefrontAccessToken: "08fa0b442045fe3a5e51a80d907caa2e",
+  domain: "chidiebere-storre.myshopify.com",
+});
